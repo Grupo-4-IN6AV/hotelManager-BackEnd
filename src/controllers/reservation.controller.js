f17517388ebeabd8583f6951cf8d95ffc5932ae7
sescobar-2020031 <sescobar-2020031@kinal.edu.gt>
2022-07-29 21:31:47 -0600

    Se agrego funcion de reservacion

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -88,13 +88,12 @@ exports.saveR = async (req, res) => {
                 return res.status(400).send({ message: 'This Room Not Available in this Dates' })
         }
 
-        const searchTypeRoom = await typeRoomModel.findOne({_id:verificRoom.typeRoom})
-            if (!searchTypeRoom) return res.status(400).send({ message: 'Room not found.' });
+        const searchTypeRoom = await typeRoomModel.findOne({ _id: verificRoom.typeRoom })
+        if (!searchTypeRoom) return res.status(400).send({ message: 'Room not found.' });
 
-            if(params.totalPersons > searchTypeRoom.numberPersons)
-            {
-                return res.status(400).send({message:'The room does not have capacity for so many people'});
-            }
+        if (params.totalPersons > searchTypeRoom.numberPersons) {
+            return res.status(400).send({ message: 'The room does not have capacity for so many people' });
+        }
 
         const setRoom = {
             room: params.room,
@@ -121,8 +120,8 @@ exports.saveR = async (req, res) => {
         const addReservation = new Reservation(checkdata);
         await addReservation.save();
         //Actualizar la fecha disponible//
-        const updateDateAvailable = await Room.findOneAndUpdate({ _id: params.room }, { dateAvailable: finishDateExit}, { new: true });
-        const updateSaleRoom = await Room.findOneAndUpdate({ _id: params.room }, {$inc:{sales:1}}, { new: true });
+        const updateDateAvailable = await Room.findOneAndUpdate({ _id: params.room }, { dateAvailable: finishDateExit }, { new: true });
+        const updateSaleRoom = await Room.findOneAndUpdate({ _id: params.room }, { $inc: { sales: 1 } }, { new: true });
 
 
         //Actualizar Estado de las Habitaciones//
@@ -192,6 +191,62 @@ exports.addService = async (req, res) => {
     }
 }
 
+exports.addServiceUser = async (req, res) => {
+    try {
+        const reservationId = req.params.id;
+        const params = req.body;
+        const user = req.user.sub;
+        const data = {
+            services: params.services
+        };
+
+        let msg = validateData(data);
+        if (msg) return res.status(400).send(msg);
+
+        for (let serviceId of services) {
+            //Verificar que exista el Servicio en la Reservación//
+            const serviceExistReservation = await Reservation.findOne({ $and: [{ _id: reservationId }, { 'services.service': serviceId }] });
+            if (serviceExistReservation) return res.status(400).send({ message: 'Service is already in this Reservation' });
+        }
+
+        const reservationExist = await Reservation.findOne({ $and: [{ _id: reservationId }, { user: user }] });
+        //Verificar que Exista la Reservación //
+        if (!reservationExist)
+            return res.status(400).send({ message: 'Reservation not found' });
+
+        for (let serviceId of services) {
+            const serviceExist = await Service.findOne({ _id: serviceId });
+            if (!serviceExist) return res.status(400).send({ message: 'Service not found' });
+
+            const setService = {
+                service: serviceId,
+                price: serviceExist.price,
+            }
+
+            const subTotal = reservationExist.subTotal + (setService.price * reservationExist.totalDays);
+
+            const IVA = parseFloat(setService.price * reservationExist.totalDays) * 0.12;
+            const IVATotal = (reservationExist.IVA) + IVA;
+            const total = (parseFloat(subTotal) + parseFloat(IVATotal));
+
+            const newReservation = await Reservation.findOneAndUpdate({ _id: reservationExist._id },
+                {
+                    $push: { service: setService },
+                    subTotal: subTotal,
+                    IVA: IVATotal,
+                    total: total
+                },
+                { new: true });
+            if (!newReservation) return res.status(400).send({ message: 'Error adding services' })
+        }
+        const reservation = await detailsShoppingCart(reservationExist._id);
+        return res.send({ message: 'Added New Service to Reservation.', reservation })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ message: 'Error adding services.' });
+    }
+}
+
 
 exports.deleteService = async (req, res) => {
     try {
@@ -219,7 +274,7 @@ exports.deleteService = async (req, res) => {
         const total = (parseFloat(subTotal) + parseFloat(IVATotal));
         const deleteServiceReservation = await Reservation.findOneAndUpdate(
             { _id: reservationID },
-            {subTotal: subTotal, IVA: IVATotal, total:total }, { new: true }).lean();
+            { subTotal: subTotal, IVA: IVATotal, total: total }, { new: true }).lean();
 
 
         for (let serv of servicesReservation) {
@@ -233,7 +288,56 @@ exports.deleteService = async (req, res) => {
                 return res.send({ message: 'Service deleted successfully ', deleteServiceReservation });
 
             }
-            if(serv.service != data.serviceID) return res.send({message: 'Service alredy delete'})
+            if (serv.service != data.serviceID) return res.send({ message: 'Service alredy delete' })
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).send({ message: 'Error deleting Match.', err });
+    }
+}
+
+exports.deleteService = async (req, res) => {
+    try {
+        const reservationID = req.params.id;
+        const params = req.body;
+        let data = {
+            serviceDelete: params.services
+        };
+
+        //Valida data obligatoria
+        let msg = validateData(data);
+        if (msg) return res.status(400).send(msg);
+
+        const reservation = await Reservation.findOne({ _id: reservationID });
+        if (!reservation) return res.status(400).send({ message: 'Reservation not found.' });
+
+        for (let serviceID of serviceDelete) {
+            const service = await Service.findOne({ _id: serviceID });
+            if (!service) return res.status(400).send({ message: 'Service not found.' });
+            const servicesReservation = reservation.services;
+
+            const subTotal = reservation.subTotal - (service.price * reservation.totalDays);
+            const IVA = parseFloat(service.price * reservation.totalDays) * 0.12;
+            const IVATotal = (reservation.IVA) - IVA;
+            const total = (parseFloat(subTotal) + parseFloat(IVATotal));
+            const deleteServiceReservation = await Reservation.findOneAndUpdate(
+                { _id: reservationID },
+                { subTotal: subTotal, IVA: IVATotal, total: total }, { new: true }).lean();
+
+
+            for (let serv of servicesReservation) {
+                if (serv.service == serviceID) {
+
+                    //Eliminar el Servicio//
+                    const deleteServiceReservation = await Reservation.findOneAndUpdate(
+                        { _id: reservationID },
+                        { $pull: { 'services': { 'service': serviceID } } }, { new: true }).lean();
+                    //Eliminar el  Servicio a la Reservación //
+                    return res.send({ message: 'Service deleted successfully ', deleteServiceReservation });
+                }
+                if (serv.service != data.serviceID) return res.send({ message: 'Service alredy delete' })
+            }
         }
     }
     catch (err) {
@@ -245,14 +349,12 @@ exports.deleteService = async (req, res) => {
 
 //Ordenar las habitaciones por Hotel//
 exports.getReservationsUser = async (req, res) => {
-    try 
-    {
+    try {
         const userID = req.user.sub
-        const reservations = await Reservation.find({user: userID }).populate('hotel room.room')
+        const reservations = await Reservation.find({ user: userID }).populate('hotel room.room')
         return res.send({ reservations })
-    } 
-    catch (err) 
-    {
+    }
+    catch (err) {
         console.log(err);
     }
 }
@@ -260,15 +362,13 @@ exports.getReservationsUser = async (req, res) => {
 
 //Ordenar las habitaciones por Hotel//
 exports.getReservationsManager = async (req, res) => {
-    try 
-    {
+    try {
         const manager = req.user.sub
-        const hotel = await Hotel.findOne({admin:manager})
-        const reservations = await Reservation.find({hotel: hotel }).populate('hotel room.room')
+        const hotel = await Hotel.findOne({ admin: manager })
+        const reservations = await Reservation.find({ hotel: hotel }).populate('hotel room.room')
         return res.send({ reservations })
-    } 
-    catch (err) 
-    {
+    }
+    catch (err) {
         console.log(err);
     }
 }
@@ -312,5 +412,5 @@ exports.deleteReservation = async (req, res) => {
     }
 
 
-    
+
 }
